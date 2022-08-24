bc7ec1b610161aa08f919103637956e762be1d40
antaler <antonioalejandroserranoramirez@gmail.com>
2022-08-24 21:27:13 +0200

    Calculadora

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,48 @@
-fn main() {
-   let mut nombre: String = String::new();
-   println!("Hola Soy {}", nombre);
+use regex::Regex;
 
-   loop {
-       std::io::stdin().read_line(&mut nombre).unwrap();
-       println!("Hola Soy {}", nombre);
+fn main() {
+
+    // Regex
+    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_mult = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+
+    // Traer datos del usuario
+    println!("Por favor uintroduce tu expresion");
+    let mut expresion = String::new();
+    std::io::stdin().read_line(&mut expresion).unwrap();
+
+    loop {
+        // Aplicar Operaciones
+        let caps = re_mult.captures(expresion.as_str());
+        if caps.is_none() {
+            break
+        }
+        let caps = caps.unwrap();
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32= caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32= caps.get(2).unwrap().as_str().parse().unwrap();
+        let mult = left_value * right_value;
+        expresion = expresion.replace(cap_expression, &mult.to_string())
+        
     }
+
+    loop {
+        // Aplicar Operaciones
+        let caps = re_add.captures(expresion.as_str());
+        if caps.is_none() {
+            break
+        }
+        let caps = caps.unwrap();
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32= caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32= caps.get(2).unwrap().as_str().parse().unwrap();
+        let addition = left_value + right_value;
+        expresion = expresion.replace(cap_expression, &addition.to_string())
+        
+    }
+    // mostrar el resuktdi
+
+
+    print!("Resultado {}", expresion)
 
 }
