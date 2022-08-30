be6ba261fb79ed832c5db93951b904f3d9b06652
antaler <antonioalejandroserranoramirez@gmail.com>
2022-08-30 18:28:43 +0200

    before funs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 
 
 fn main() {
-    
+
     let mut nombres : Vec<String> = Vec::new();
+    for _i in 0..3 {
+        println!("Por favor introduce un nombre");
+        let mut nombre  = String::new();
+        std::io::stdin().read_line(&mut nombre).unwrap();
+        
+        nombres.push(nombre.trim().to_string());
+    }
+    
+    for nombre in nombres {
+        println!("El nombre es: {}", nombre)
+    }
 
-    println!("Por favor introduce un nombre");
-    let mut nombre  = String::new();
-    std::io::stdin().read_line(&mut nombre).unwrap();
+    let  hola = ["H", "O", "L", "A"];
 
-
+    println!("{}",hola[0]);
+    println!("{}",hola[1]);
 
 }
